b054527ad273fb1d825e15f83689e2650e9f2845
Barkin Simsek <barkin.simsek@hotmail.com>
2019-06-24 16:14:02 +0400

    abstract classes

--- benchmark-server-0.3/benchy/build/CMakeFiles/benchy.exe.dir/DependInfo.cmake
+++ benchmark-server-0.3/benchy/build/CMakeFiles/benchy.exe.dir/DependInfo.cmake
@@ -10,10 +10,16 @@ set(CMAKE_DEPENDS_CHECK_CXX
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "FFT_NATIVE"
+  )
+
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
   ".././include"
   ".././src"
+  "/home/woswos/HElib/build/helib_pack/include"
   )
 
 # Targets to which this target links.

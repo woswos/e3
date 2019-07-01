3ef38e3aa2b52dfb8306454d30f94b25ffeddcdf
Your Name <you@example.com>
2019-07-01 07:46:46 +0400

    z

--- benchmark-server/oleg/oleg.cpp
+++ benchmark-server/oleg/oleg.cpp
@@ -1,40 +1,15 @@
-#include <string>
 #include <iostream>
 
-class Ciphertext
-{
+#include "oleg.h"
 
-private:
-    int x = 0;
 
-public:
-    Ciphertext(){};
-
-    Ciphertext(std::string value){
-        if(value == "0"){
-            x = 0;
-        } else {
-            x = 1;
-        }
-    }
-
-    std::string str() const {
-        return std::to_string(x);
-    }
-
-    static Ciphertext nand(Ciphertext a, Ciphertext b) {
-        Ciphertext r;
-        r.x = 1 & ~(a.x&b.x);
-        return r;
-    }
-
-};
+using Bit = CiphertextBit;
 
 int main(int argc, char const *argv[]) {
 
-    Ciphertext a("1"), b("0");
+    Bit a("1"), b("0");
 
-    auto c = Ciphertext::nand(a, b);
+    auto c = Bit::nand(a, b);
 
     std::cout << c.str() << '\n';
 
---
+++ benchmark-server/oleg/oleg.h
@@ -0,0 +1,31 @@
+#include <string>
+
+class CiphertextBit
+{
+
+private:
+    int x = 0;
+
+public:
+    CiphertextBit(){};
+
+    CiphertextBit(std::string value){
+        if(value == "0"){
+            x = 0;
+        } else {
+            x = 1;
+        }
+    }
+
+    std::string str() const {
+        return std::to_string(x);
+    }
+
+    static CiphertextBit nand(CiphertextBit a, CiphertextBit b) {
+        CiphertextBit r;
+        r.x = 1 & ~(a.x&b.x);
+        return r;
+    }
+
+};
+

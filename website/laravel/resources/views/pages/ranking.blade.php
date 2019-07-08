361bd0541e2a11ee4c15fed5eee4a91acdf48b88
woswos <barkin.simsek@hotmail.com>
2019-07-08 14:04:18 -0700

    dynaÃmic graph titles

--- website/laravel/resources/views/pages/ranking.blade.php
+++ website/laravel/resources/views/pages/ranking.blade.php
@@ -9,14 +9,14 @@
         <table style="width:100%;">
             <tr>
                 <td style="vertical-align: top;">
-                    <div class="list-group" id="graph_group">
+                    <div class="list-group" id="graph_group_">
                         @foreach ($chart_values as $key => $value)
-                            <a href="javascript:;" id="{{ $key }}" class="list-group-item list-group-item-action @if(($loop->first)) active @endif">{{ucfirst( $key )}}</a>
+                            <a href="javascript:;" id="{{ $key }}" class="list-group-item button_track list-group-item-action @if(($loop->first)) active @endif">{{ucfirst( $key )}}</a>
                         @endforeach
                     </div>
                 </td>
                 <td style="padding-left:10px; vertical-align: top">
-                    <canvas id="ranking_chart" width="%100" height="100px"></canvas>
+                    <canvas id="ranking_chart" width="%100" ></canvas>
                     @include('partials/chart')
                 </td>
             </tr>
@@ -26,17 +26,17 @@
 
     <script>
     // Get the container element
-    var btnContainer = document.getElementById("graph_group");
+    var btnContainer = document.getElementById("graph_group_");
 
     // Get all buttons with class="btn" inside the container
-    var btns = btnContainer.getElementsByClassName("list-group-item");
+    var btns = btnContainer.getElementsByClassName("button_track");
 
     // Loop through the buttons and add the active class to the current/clicked button
     for (var i = 0; i < btns.length; i++) {
       btns[i].addEventListener("click", function() {
-        var current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace(" active", "");
-        this.className += " active";
+        var current = document.getElementsByClassName("list-group-item button_track list-group-item-action active");
+        current[0].className = "list-group-item button_track list-group-item-action";
+        this.className = "list-group-item button_track list-group-item-action active";
       });
     }
     </script>
--- website/laravel/resources/views/partials/chart.blade.php
+++ website/laravel/resources/views/partials/chart.blade.php
@@ -1,6 +1,8 @@
 <script>
 var chart_labels = [''];
 var benchmark_data = [{ x: 0, y: 0 }];
+var yLabel = "";
+var xLabel = "";
 
 var ctx = document.getElementById("ranking_chart").getContext('2d');
 var config = {
@@ -25,14 +27,14 @@ var config = {
           yAxes: [{
             scaleLabel: {
               display: true,
-              labelString: '32 Bit Integer Division Speed (ns)'
+              labelString: yLabel
             }
           }],
 
           xAxes: [{
             scaleLabel: {
               display: true,
-              labelString: 'Prize ($)'
+              labelString: xLabel
             }
           }]
         },
@@ -57,6 +59,10 @@ var ranking_chart = new Chart(ctx, config);
     foreach ($chart_values as $key => $scheme) {
 
         echo "$(document).ready(function() {";
+            echo "var yLabel = '".ucfirst($key)." Speed (ms)';
+                  var xLabel = 'Prize ($)';
+                 ";
+
             echo " var chart_labels = [";
                     foreach ($scheme as $scheme_title => $speed_and_prize) {
                         echo "'".$scheme_title."',";
@@ -70,9 +76,10 @@ var ranking_chart = new Chart(ctx, config);
             echo "]; ";
 
             echo "
-                  var data = ranking_chart.config.data;
-                  data.datasets[0].data = benchmark_data;
-                  data.labels = chart_labels;
+                  ranking_chart.config.data.datasets[0].data = benchmark_data;
+                  ranking_chart.config.data.labels = chart_labels;
+                  ranking_chart.config.options.scales.yAxes[0].scaleLabel.labelString = yLabel;
+                  ranking_chart.config.options.scales.xAxes[0].scaleLabel.labelString = xLabel;
                   ranking_chart.update();
                  ";
         echo "
@@ -86,25 +93,30 @@ var ranking_chart = new Chart(ctx, config);
     foreach ($chart_values as $key => $scheme) {
 
         echo " $('#".$key."').click(function() { ";
-        echo " var chart_labels = [";
-                foreach ($scheme as $scheme_title => $speed_and_prize) {
-                    echo "'".$scheme_title."',";
-                }
-        echo "]; ";
+            echo "var yLabel = '".ucfirst($key)." Speed (ms)';
+                  var xLabel = 'Prize ($)';
+                 ";
 
-        echo "var benchmark_data = [";
-                foreach ($scheme as $scheme_title => $speed_and_prize) {
-                    echo "{ x: ".$speed_and_prize['prize']." , y: ".$speed_and_prize['speed']." },";
-                }
-        echo "]; ";
+            echo " var chart_labels = [";
+                    foreach ($scheme as $scheme_title => $speed_and_prize) {
+                        echo "'".$scheme_title."',";
+                    }
+            echo "]; ";
 
-        echo "
-              var data = ranking_chart.config.data;
-              data.datasets[0].data = benchmark_data;
-              data.labels = chart_labels;
-              ranking_chart.update();
-              });
-             ";
+            echo "var benchmark_data = [";
+                    foreach ($scheme as $scheme_title => $speed_and_prize) {
+                        echo "{ x: ".$speed_and_prize['prize']." , y: ".$speed_and_prize['speed']." },";
+                    }
+            echo "]; ";
+
+            echo "
+                  ranking_chart.config.data.datasets[0].data = benchmark_data;
+                  ranking_chart.config.data.labels = chart_labels;
+                  ranking_chart.config.options.scales.yAxes[0].scaleLabel.labelString = yLabel;
+                  ranking_chart.config.options.scales.xAxes[0].scaleLabel.labelString = xLabel;
+                  ranking_chart.update();
+                  });
+                 ";
     }
 
 @endphp
